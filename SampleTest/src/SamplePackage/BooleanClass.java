0e933955d5500a0b0b50aa50383909a2f51968d0
LavetiVenkata <dsatyaveni@gmail.com>
2018-11-13 08:33:37 +0530

    na

---
+++ SampleTest/src/SamplePackage/BooleanClass.java
@@ -0,0 +1,14 @@
+package SamplePackage;
+
+public class BooleanClass {
+	
+	public static void main (String args[]){
+		
+		boolean blean = true;
+		System.out.println("the value of boolean variable is - " + blean);
+		
+		boolean a = 10> 20;
+		System.out.println(a);
+	}
+
+}
---
+++ SampleTest/src/SamplePackage/CharClass.java
@@ -0,0 +1,11 @@
+package SamplePackage;
+
+public class CharClass {
+	
+	public static void main (String args[]){
+		
+		char a = 's';
+		System.out.println("The value of a char variable is - " + a);
+	}
+
+}
